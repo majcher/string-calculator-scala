1a78ba9d7ee9b8058c4dd911d5eabf74239e4196
Michał Majcherski <mmajcherski@gmail.com>
2014-02-07 23:22:14 +0100

    Custom delimiters implementation

--- src/main/scala/pl/mmajcherski/stringcalculator/StringCalculator.scala
+++ src/main/scala/pl/mmajcherski/stringcalculator/StringCalculator.scala
@@ -1,9 +1,20 @@
 package pl.mmajcherski.stringcalculator
 
+import com.google.common.base.Splitter
+import scala.collection.JavaConversions._
+
 class StringCalculator {
 
+	private val DefaultForEmptyInput: String = "0"
+	private val FirstLineStartString: String = "//"
+	private val DefaultSplitPattern: String = "[,\n]"
+
 	def add(input : String) : Integer = {
-		if (input.isEmpty) 0 else input.split("[,\n]").map(_.toInt).sum
+		val nonEmptyInput = if (input.isEmpty) DefaultForEmptyInput else input
+		val customDelimiter = if (input.startsWith(FirstLineStartString)) new Some(input.charAt(2)) else None
+		val nonEmptyInputWithoutDelimiter = if (customDelimiter.isDefined) nonEmptyInput.substring(4) else nonEmptyInput
+		val splitter = if (customDelimiter.isDefined) Splitter.on(customDelimiter.get) else Splitter.onPattern(DefaultSplitPattern)
+		splitter.split(nonEmptyInputWithoutDelimiter).toList.map(_.toInt).sum
 	}
 
 }
