b824918fff5dc356673a245e054d893432096eca
Michał Majcherski <mmajcherski@gmail.com>
2014-02-07 22:06:06 +0100

    Two digits input impl.

--- src/main/scala/pl/mmajcherski/stringcalculator/StringCalculator.scala
+++ src/main/scala/pl/mmajcherski/stringcalculator/StringCalculator.scala
@@ -3,7 +3,7 @@ package pl.mmajcherski.stringcalculator
 class StringCalculator {
 
 	def add(input : String) : Integer = {
-		if (input.isEmpty) 0 else input.toInt
+		if (input.isEmpty) 0 else input.split(',').map(_.toInt).sum
 	}
 
 }
