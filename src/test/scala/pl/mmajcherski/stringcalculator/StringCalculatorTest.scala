172ac13cdc117e52c7bcb4cf05d29f02de813dc3
Michał Majcherski <mmajcherski@gmail.com>
2014-02-07 22:12:03 +0100

    Multiple comma-delimited numbers input test.

--- src/test/scala/pl/mmajcherski/stringcalculator/StringCalculatorTest.scala
+++ src/test/scala/pl/mmajcherski/stringcalculator/StringCalculatorTest.scala
@@ -6,19 +6,24 @@ class StringCalculatorTest extends FlatSpec with Matchers {
 
 	val calc = new StringCalculator()
 
-	"String calculator" should "return 0 for empty input string" in {
+	"String calculator" should "provide sum equal to zero for empty string" in {
 		val result = calc.add("")
 		result should equal (0)
 	}
 
-	it should "return 1 for '1' input" in {
+	it should "provide sum equal to single number passed as string" in {
 		val result = calc.add("1")
 		result should equal (1)
 	}
 
-	it should "return 3 for '1,2' input" in {
+	it should "provide sum of two comma-delimited numbers" in {
 		val result = calc.add("1,2")
 		result should equal (3)
+	}
+
+	it should "provide sum of any comma-delimited numbers" in {
+		val result = calc.add("1,2,3,4,5")
+		result should equal (15)
 	}
 
 }
